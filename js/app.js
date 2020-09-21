c72911eb0982fdcb21b6a16f516f1bddae664b65
hasan.hawar@powercoders.org <hasan.hawar@powercoders.org>
2020-09-21 12:53:41 +0200

    feat: add the dom Strings and setup the EventListner functions place

--- js/app.js
+++ js/app.js
@@ -4,7 +4,26 @@ let budgetController = (function () {
 })();
 // UI Controller
 let UIController = (function () {
+  let DOMStrings = {
+      inputType : '.add__type',
+      inputDescription: '.add__description',
+      inputValue: '.add__value',
+      inputBtn: '.add__btn'
+
+  };
 })();
 // Global Conroller
 let Controller = (function (budgetCtrl, UICtrl) {
+
+    let setupEventListiner = function(){
+
+        let Dom = UICtrl.getDomStrings();
+
+        document.querySelector(Dom.inputBtn).addEventListener("click", ctrlAddItem);
+        document.addEventListener("keypress", function (event) {
+            if(event.keyCode === 13 || event.which === 13){
+              ctrlAddItem()
+            }
+        });
+    }
 })(budgetController, UIController);
