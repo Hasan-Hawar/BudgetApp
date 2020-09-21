573fcacc91ad1c95148f5de03cd6edcc42272ca5
hasan.hawar@powercoders.org <hasan.hawar@powercoders.org>
2020-09-21 12:52:02 +0200

    feat: structure the project code and add all needed controllers

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Budget Controller
+let budgetController = (function () {
+  // some code
+})();
+// UI Controller
+let UIController = (function () {
+})();
+// Global Conroller
+let Controller = (function (budgetCtrl, UICtrl) {
+})(budgetController, UIController);
