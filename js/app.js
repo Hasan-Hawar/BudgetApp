b72f3e8e4b94742425d20ec14e5e3924d8406715
hasan.hawar@powercoders.org <hasan.hawar@powercoders.org>
2020-09-29 17:06:11 +0200

    feat: repalce the placeholder with the actual text. add the dom string to the html

--- js/app.js
+++ js/app.js
@@ -65,6 +65,18 @@ let UIController = (function () {
         html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
   }
+
+      // Replace the placeholder text with an actual text
+
+      newHtml = html.replace('%id%',obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value)
+
+
+      // Insert the HTML Tag into the DOM
+
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
 })();
 
 // Global Conroller
